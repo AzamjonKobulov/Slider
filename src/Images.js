06c5bc4f3350d5a0cea89b22b1c8b24b2c158cce
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-10-19 13:52:52 +0500

    added

---
+++ src/Images.js
@@ -0,0 +1,6 @@
+const images = [
+  'https://images.pexels.com/photos/327434/pexels-photo-327434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/533854/pexels-photo-533854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/389724/pexels-photo-389724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/433524/pexels-photo-433524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+];
